57759a7bb2afd27098b5e4b554f69c25f7b884c8
reshad21 <miakala7@gmail.com>
2023-08-16 17:29:39 +0600

    skills component create

--- src/components/Profile/About.jsx
+++ src/components/Profile/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div>
             <div className="p-5 border-2 border-slate-300 rounded-md mb-5">
                 <div className="flex justify-between items-center mb-4">
-                    <h3 className='text-[#222222]'>About Vishnu</h3>
+                    <h3 className='text-[#222222] font-bold'>About Vishnu</h3>
                     <Link className='bg-[#F0EFFA] text-[12.67px] px-5 py-2 rounded-2xl text-[#222]'>Edit</Link>
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam tenetur eius aperiam quidem. Vitae rerum a itaque? Sequi, eligendi ex.</p>
